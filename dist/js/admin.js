3d78108bedd9a41f55d71ba675f16e9e29b32473
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-01-03 16:58:37 +0700

    changed active class toggle on menu click event

--- dist/js/admin.js
+++ dist/js/admin.js
@@ -32,7 +32,7 @@ $(window).scroll(function(event){
 $('ul.nav-menu').on('click', '.dropdown', function() {
     $(this).find('.dropdown-menu').toggleClass('show', 300);
     $(this).find('.dropdown-menu').toggleClass('hide', 300);
-    $(this).find('.dropdown-menu').prev().toggleClass('active-open');
+    $(this).find('.dropdown-menu').prev().toggleClass('active');
 })
 
 $('.sidebar.collapse li.dropdown').hover( function() {
